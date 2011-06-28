0e55d45c54eb56920451487f07bf35a94a86df69
Micah Warren <micahw@lordnull.com>
2011-06-28 09:36:08 -0500

    Altered build to conform to most recent OpenACD plugin

--- post_compile.sh
+++ post_compile.sh
@@ -11,6 +11,14 @@ do
 	fi
 done
 
+rm -rf plugin
+mkdir plugin
+mkdir plugin/oacd_hop
+cp -R ebin include priv plugin/oacd_hop
+cp -R contrib/rabbitmq-erlang-client/dist/amqp_client-0.0.0 \
+	contrib/rabbitmq-erlang-client/dist/rabbit_common-0.0.0 \
+	plugin
+
 echo "***************************************************************"
 echo "* Compile success!                                            *"
 echo "*                                                             *"
